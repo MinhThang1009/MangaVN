c3c958d74e0da5ed3ebd251f90b9218bd04f8c1a
MihnixArch0n <189517061+MihnixArch0n@users.noreply.github.com>
2026-05-14 21:38:33 +0700

    chore: cleanup unused resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,8 +70,8 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.core)
 
     testImplementation(libs.junit)
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
-    testImplementation("io.mockk:mockk:1.13.13")
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.mockk)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
