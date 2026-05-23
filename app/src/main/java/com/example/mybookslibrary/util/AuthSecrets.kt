b45b1686ef6ab7aae715e4c767d7845be29b1986
Phan Tho <phantho.vt.vp@gmail.com>
2026-05-24 00:12:53 +0700

    rm auth secret

--- app/src/main/java/com/example/mybookslibrary/util/AuthSecrets.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mybookslibrary.util
-
-object AuthSecrets {
-    // Replace with your Web Client ID from Google Cloud Console
-    const val WEB_CLIENT_ID = "YOUR_WEB_CLIENT_ID_HERE"
-}
